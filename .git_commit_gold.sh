fe6454af0ca5a5db6f7e8df46b42db812d0ef97c
noahrd0 <no.radal1203@gmail.com>
2026-07-02 17:14:03 +0200

    Retire script git temporaire

--- .git_commit_gold.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /mnt/c/Users/33650/Downloads/ipssi_big_data_entreprise/big_data_entreprise
-git add -A
-git status
-git commit -m "Jour 3 : couche Gold, API FastAPI et frontend React"
-git push -u origin gold-layer
