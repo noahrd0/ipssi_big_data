1e7ece8ef96f1aebcf419e2cc435d50bee9f7fe7
noahrd0 <no.radal1203@gmail.com>
2026-07-02 17:14:09 +0200

    Retire script cleanup

--- _cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /mnt/c/Users/33650/Downloads/ipssi_big_data_entreprise/big_data_entreprise
-git rm -f .git_commit_gold.sh 2>/dev/null || true
-git add -A
-git diff --cached --quiet || git commit -m "Retire script git temporaire"
-git push
